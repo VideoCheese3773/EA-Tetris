19ece876bd727cf8c40e32c80c097da7d078bc17
not gonna tell ya <eedduxx@gmail.com>
2023-10-21 21:49:08 -0500

    added arduino interaction

---
+++ index.js
@@ -0,0 +1,29 @@
+import { SerialPort } from 'serialport'
+import { ReadlineParser } from 'serialport'
+
+//const SerialPort = require(serialport)
+
+const protocolConfiguration = {
+   path: 'COM3',
+   baudRate: 9600
+}
+// list serial ports:
+SerialPort.list().then (
+    ports => ports.forEach(port =>console.log(port.path)),
+    err => console.log(err)
+ ) 
+ 
+const port = new SerialPort(protocolConfiguration);
+const parser = port.pipe(new ReadlineParser());
+
+parser.on('data', (data) => {    
+   console.log(data);
+
+   /*if( parseInt(data) % 5 == 0){
+      parser.write("encender\n");
+   } else {
+      parser.write("apagar\n");
+   }*/
+
+   //parser.write("Orale\n");
+});
